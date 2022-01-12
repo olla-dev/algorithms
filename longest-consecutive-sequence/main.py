362436b6dcdb364dc235195ad0e083d60eb55b7b
hamza <hamza@codeplumbers.eu>
2022-01-12 15:31:21 +0100

    fixed logic

--- longest-consecutive-sequence/main.py
+++ longest-consecutive-sequence/main.py
@@ -1,18 +1,16 @@
 import unittest
 
 def solve(A):
-    n = len(A)
-
-    if n < 2: 
-        return 1
-    else: 
-        a_set = set(A)
-        longest = 1
-        for i in A:
-            # check i - 1 is in set?
-            if i - 1 in a_set:
-                longest += 1
-        return longest
+    a_set = set(A)
+    longest = 0
+    for i in A:
+        # check i - 1 is in set?
+        if i - 1 not in a_set:
+            j = i
+            while j in a_set:
+                j += 1
+            longest = max(longest, j - i)
+    return longest
 
 
 if __name__ == '__main__': 
@@ -20,8 +18,8 @@ if __name__ == '__main__':
     assert solve(A) == 1   
     A = [2,4]
     assert solve(A) == 1  
-    A = [7,100,6,5,2,1]
-    assert solve(A) == 4
+    A = [7,100,6,5,2,1,3]
+    assert solve(A) == 3
     B = [100,4,200,1,3,2]
     assert solve(B) == 4
     C = [0,3,7,2,5,8,4,6,0,1]
